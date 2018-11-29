ad670177a3ce3acfbabae61de4eaf565c0c8f398
htao <htao@thoughtworks.com>
2018-11-29 10:07:11 +0800

    [taohui] use cardSection to style each part

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => (
-        <View>
-            <Text>{props.record.title}</Text>
-        </View>
+const AlbumDetail = ({ record: { title, artist, thumbnail_image } }) => (
+        <Card>
+            <CardSection>
+                <View>
+                    <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+                </View>
+                <View style={styles.textContainerStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+        </Card>
     );
+
+const styles = {
+    textContainerStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+        width: 50,
+        height: 50
+    }
+};
 
 export default AlbumDetail;
