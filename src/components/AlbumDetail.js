dc5b5116a8814d920b75057a5f8a035f3c9349a4
htao <htao@thoughtworks.com>
2018-12-15 18:02:37 +0800

    [taohui] display album image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -3,28 +3,59 @@ import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = ({ record: { title, artist, thumbnail_image } }) => (
-        <Card>
-            <CardSection>
-                <View>
-                    <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
-                </View>
-                <View style={styles.textContainerStyle}>
-                    <Text>{title}</Text>
-                    <Text>{artist}</Text>
-                </View>
-            </CardSection>
-        </Card>
-    );
+const AlbumDetail = ({ record }) => {
+    const {
+        title,
+        artist,
+        thumbnail_image,
+        image
+    } = record;
+    const { 
+        thumbnailContainerStyle, 
+        thumbnailStyle,
+        imageStyle, 
+        textContainerStyle,
+        headerTextStyle } = styles;
+    return (
+    <Card>
+        <CardSection>
+            <View style={thumbnailContainerStyle}>
+                <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+            </View>
+            <View style={textContainerStyle}>
+                <Text style={headerTextStyle}>{title}</Text>
+                <Text>{artist}</Text>
+            </View>
+        </CardSection>
+
+        <CardSection>
+            <Image style={imageStyle} source={{ uri: image }} />
+        </CardSection>
+    </Card>);
+};
 
 const styles = {
     textContainerStyle: {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
+    headerTextStyle: {
+        fontSize: 18
+    },
     thumbnailStyle: {
         width: 50,
         height: 50
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
     }
 };
 
